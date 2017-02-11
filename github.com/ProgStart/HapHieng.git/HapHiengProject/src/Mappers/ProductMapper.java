27d64abc125e7f1af044a2f465a064c5fc43235d
Seiji Ralph Villafranca <seijiv13@outlook.com>
2017-02-11 23:43:11 +0800

    Committing Changes
    >> Added Product Model and Product Implem

--- github.com/ProgStart/HapHieng.git/HapHiengProject/src/Mappers/ProductMapper.java
+++ github.com/ProgStart/HapHieng.git/HapHiengProject/src/Mappers/ProductMapper.java
@@ -1,5 +1,11 @@
 package Mappers;
 
-public interface ProductMapper {
+import java.util.ArrayList;
 
+public interface ProductMapper {
+     
+	public ArrayList<String> getAllProducts(String product_line, String category);
+	public ArrayList<String> getAllCategories();
+	public ArrayList<String> getAllProductLine();
+	
 }
